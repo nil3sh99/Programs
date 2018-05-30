68f384ebc95e1429e570362268881ede3c0780c3
Nilesh <agnilesh8199@gmail.com>
2018-05-30 23:00:25 +0530

    Update speech_recog.py

--- recog/speech_recog.py
+++ recog/speech_recog.py
@@ -12,3 +12,20 @@ try:
 
 except:
 	pass
+
+#if we have an audio file already and want to covert that audio file into text then...
+
+#import speech_recognition as sr
+
+#AF = ("file_destination")
+#r = sr.Recognizer()
+
+#with sr.AudioFile(AF) as source:
+#	print('Given file says: \n')
+#	audio = r.record(source)
+
+#try:
+#	print('I detected this: ' + r.recognize_google(audio))
+
+#except:
+#	pass
