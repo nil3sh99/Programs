a16db6118fd745d72d17f7792023358e3ba5046d
Nilesh <agnilesh8199@gmail.com>
2018-06-24 22:19:38 +0530

    object type 2

---
+++ object_meth2.py
@@ -0,0 +1,17 @@
+#declaring object usin gdynamic programming
+class Student(object):
+    name = ""
+    age = 0
+    major = ""
+
+def make_student(name,age,major):
+    student = Student()
+    student.name = name
+    student.age = age
+    student.major = major
+
+    #if i want to add an extra variable then i dont have to define in this 
+    #type of object declaration
+
+    student.gpa = float(4.0)
+    return student
