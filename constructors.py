0099eb469441db63648e7917a82e98d0b344a35a
Nilesh <agnilesh8199@gmail.com>
2018-06-24 22:30:10 +0530

    parametrized constructor

---
+++ constructors.py
@@ -0,0 +1,14 @@
+#parametrized constructor
+
+class constructor:
+    num = 123
+
+    def __init__(self, data): #it is a default constructor which by default called when an object is created
+        self.num = data
+
+    def read_data(self):
+        print(self.num)
+
+obj = constructor(55)
+
+obj.read_data()
