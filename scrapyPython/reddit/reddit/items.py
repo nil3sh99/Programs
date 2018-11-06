3e78556281888f19395bfd4d97eb23c157300ae0
Nilesh <agnilesh8199@gmail.com>
2018-11-06 17:38:07 +0530

    final commit

--- scrapyPython/reddit/reddit/items.py
+++ scrapyPython/reddit/reddit/items.py
@@ -11,4 +11,6 @@ import scrapy
 class RedditItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-    pass
+    titles = scrapy.Field()
+    hrefs = scrapy.Field()
+    scores = scrapy.Field()
--- scrapyPython/reddit/reddit/spiders/reddit_job.py
+++ scrapyPython/reddit/reddit/spiders/reddit_job.py
@@ -1,13 +1,22 @@
 # -*- coding: utf-8 -*-
 import scrapy
 
-
+from reddit.items import RedditItem
 class RedditJobSpider(scrapy.Spider):
     name = 'reddit_job'
     allowed_domains = ['reddit.com']
     start_urls = ['https://www.reddit.com/r/funny/']
 
     def parse(self, response):
-        print(response.css('a.s1cbwutf-1'))
-        print(response.css('a.s1cbwutf-1').extract())
-        print(response.css('a.s1cbwutf-1.attr(href)').extract)
+        titles = response.css('a.s1cbwutf-1')
+        hrefs = response.css('a.s1cbwutf-1').extract()
+        scores = response.css('a.s1cbwutf-1.attr(href)').extract()
+
+        for item in zip(titles, hrefs, scores):
+
+            new_item = RedditItem()
+
+            new_item['title'] = item[0]
+            new_item['href'] = item[1]
+            new_item['score'] = item[2]
+            yield new_item
