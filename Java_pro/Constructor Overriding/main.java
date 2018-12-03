db43b11be624e957118dfea30be945be0825ec27
Nilesh <agnilesh8199@gmail.com>
2018-12-03 18:21:26 +0530

    constructor overrding

---
+++ Java_pro/Constructor Overriding/main.java
@@ -0,0 +1,28 @@
+class student{
+	 int id;
+	 int roll_no;
+	 String name;	
+	
+	student(int i, String s){
+	id = i;
+	name = s;
+	}
+	student(int i, String s, int r){
+	id = i;
+	name = s;
+	roll_no = r;
+	}
+	void display(){
+		System.out.println(id + name + roll_no);
+	}
+}
+
+class main{
+	public static void main(String ar[]){
+	student s1 = new student(1, "A");
+	student s2 = new student(2, "B", 201);
+//	System.out.println(id + name + roll_no);
+	s1.display();
+	s2.display();
+	}
+}
