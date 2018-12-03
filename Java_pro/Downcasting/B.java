71680b503e9bc5abdb1c18c7f4ceb17a9fc114e1
Nilesh <agnilesh8199@gmail.com>
2018-12-04 01:01:35 +0530

    Compile time error during downcasting

---
+++ Java_pro/Downcasting/B.java
@@ -0,0 +1,15 @@
+class A{
+	void fun1(){
+		System.out.println("Hello i am from A");
+	}
+}
+
+class B extends A{
+	void fun1(){
+		System.out.println("Hello i am from B");
+	}
+public static void main(String ar[]){
+		B b = (B)new A(); // downcasting Compile Time Error
+		b.fun1();
+	}
+}	
