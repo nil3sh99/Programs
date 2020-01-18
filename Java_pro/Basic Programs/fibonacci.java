bb85be5f95225cfe9fa3e7b04fa549e91fed1634
Nilesh <agnilesh8199@gmail.com>
2020-01-18 10:31:17 +0530

    changes

---
+++ Java_pro/Basic Programs/fibonacci.java
@@ -0,0 +1,14 @@
+public class fibonacci{
+public static void main(String args[])
+    {
+        int n1=0, n2=1, n3, i;
+        System.out.print(n1+ "" + n2);
+        for(i=2; i<=5; i++)
+        {
+            n3 = n1 + n2;
+            System.out.print(n3);
+            n1 = n2;
+            n2 = n3;
+        }
+    };
+}
