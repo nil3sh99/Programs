969e58199bb10dc1d38eb2889eba08ff59573c1b
Nilesh <agnilesh8199@gmail.com>
2020-01-18 11:41:58 +0530

    Factorial of number

---
+++ Java_pro/Basic Programs/factorial.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class factorial{
+    public static void main(String args[]){
+        System.out.print("Enter the number: ");
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int i,res=1;
+        for(i = a; i>0; i--){
+            res = res*i;
+        }
+        System.out.print(res);
+
+    };
+}
